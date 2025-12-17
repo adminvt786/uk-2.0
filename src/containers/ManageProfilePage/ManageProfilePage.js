d0755c8ca2623cfbe0763fc5e60d2611a8146d64
dev-aomine <155187697+dev-aomine@users.noreply.github.com>
2025-12-18 01:52:54 +0530

    feat: redirect travelbrand users to LandingPage in ManageProfilePage

--- src/containers/ManageProfilePage/ManageProfilePage.js
+++ src/containers/ManageProfilePage/ManageProfilePage.js
@@ -1,17 +1,23 @@
 import { useIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
-import { IconSpinner, LayoutSingleColumn, Page } from '../../components';
+import { IconSpinner, LayoutSingleColumn, NamedRedirect, Page } from '../../components';
 import { isScrollingDisabled } from '../../ducks/ui.duck';
 import TopbarContainer from '../TopbarContainer/TopbarContainer';
 import { profileListingInProgressSelector, profileListingSelector } from './ManageProfilePage.duck';
 import css from './ManageProfilePage.module.css';
 import ProfileWizard from './ProfileWizard/ProfileWizard';
+import { currentUserTypeSelector } from '../../ducks/user.duck';
 
 function ManageProfilePage(props) {
   const intl = useIntl();
   const scrollingDisabled = useSelector(isScrollingDisabled);
   const profileListing = useSelector(profileListingSelector);
   const profileListingInProgress = useSelector(profileListingInProgressSelector);
+  const currentUserType = useSelector(currentUserTypeSelector);
+
+  if (currentUserType === 'travelbrand') {
+    return <NamedRedirect name="LandingPage" />;
+  }
 
   return (
     <Page
